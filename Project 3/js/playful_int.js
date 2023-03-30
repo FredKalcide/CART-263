a3524ab7d437860e6a1ce830404beaac27232be7
FredKalcide <fredkalcide@hotmail.com>
2023-03-30 13:33:33 -0400

    notification fx
    working on the notification on hit effect

--- Project 3/js/playful_int.js
+++ Project 3/js/playful_int.js
@@ -4,8 +4,18 @@
 let cam;
 let colorToFind;
 let colorrange = 12
-let step = 0 // used for drawing the first target
+let step = 0 // used for triggering the apparition of all targets
+let currentTimeElapsed;
+let keyToStart = 0 // using this to start the game on a key press
+
+let hitTimer; //the timer that determines if a target hit is fast or not, displaying text appropriately  
+
+
+// object variables
 let Targets
+
+
+// the limits of the search region for findColor
 let xminthresh;// the limits of the search region for findColor
 let xmaxthresh;
 let yminthresh;
@@ -24,14 +34,18 @@ function setup(){
 
 
 function draw(){
+  
     translate (width, 0);
     scale(-1,1) ;
     image(cam, 0, 0, 640, 400)
   
 //let firstPx = findColor(cam, colorToFind, colorrange) // this variable is used to have an x and y position to attach to something when the color we want is found
-
-Targets.drawTarget1(); // calling our target object's methods
+if (keyToStart === 1){
+Targets.drawTarget1();
+}
+// calling our target object's other methods, they are ready to run once the first does
 Targets.drawTarget2();
+Targets.greatNotif()
 
 /*if (firstPx !== undefined){ // draw a circle filled with the color being searched for on the pixel that color is found in.
     fill(colorToFind)
@@ -45,6 +59,14 @@ Targets.drawTarget2();
 
 }
 
+function keyPressed(){ //using keyPressed to start the game
+    currentTimeElapsed = millis() // the key press triggers the storage of the current value of millis, thus the amount of time that has passed
+   // // // // // we need to have each hit of a target trigger the storage of the current elapsed time, so that it then can be subtracted from the value of millis thats constantly updated in draw, that will fix the "GREAT" / "MISS" text buggyness
+    currentTimeElapsed; //calling millis
+    print(currentTimeElapsed)
+    keyToStart = 1 // starting the game
+    // // // // //using keycodes, we can set keys to start and reset the game. like, one key sets key to start to 0, thus when it turns to 1 again the whole code runs back from the beginning
+}
 
 function mousePressed(){
     loadPixels();
@@ -92,12 +114,23 @@ class Target {
     constructor(){
         this.hg = 40// height of rectangle
         this.wd = 30// width of rectangle
-        this.x = width - 40//x position
-        this.y = height/2//y position
+        this.targetx = width - 40//x position
+        this.targety = height/2//y position
+        this.notifX = width - 40
+        this.notifY = height/2
         this.fill = colorToFind // the color of the square is the color being tracked
+        this.notiftext;
+        this.notiftextr;
+        this.notiftextg;
+        this.notiftextb;
+        this.notiftextstrokeweight = 3
+        this.notiftextop = 255
+        this.textsize = 30
     }
 
     drawTarget1(){
+        hitTimer = millis() - currentTimeElapsed
+        print (hitTimer)
         //we define the regions to find a color in by changing our universal threshold variables searching 
         xminthresh = 590//
         xmaxthresh = 640//searching for color on the right side
@@ -105,27 +138,60 @@ class Target {
         ymaxthresh = 250//
         if (findColor(cam, colorToFind, colorrange) === true){// if the color we are looking has been found within the region on the right, we draw the square on the left
             //fill(this.fill) // trying to set the color of the square to be the color being tracked, wont work somehow
-            rect (this.x, this.y, this.wd, this.hg)
+            rect (this.targetx, this.targety, this.wd, this.hg)
             step = 1
             print ("color found!");
         }else if( step === 0){// otherwise if the page has just loaded, draw the square to the left only
-            rect (this.x, this.y, this.wd, this.hg) 
+            rect (this.targetx, this.targety, this.wd, this.hg) 
         }  
-}
+    }
     
     
-    drawTarget2(){ 
+    drawTarget2(){
+        hitTimer = 0
+        hitTimer = millis() - currentTimeElapsed
+        print (hitTimer)
+ 
         xminthresh = 0//
         xmaxthresh = 160// searching for color on the left
         yminthresh = 200//
         ymaxthresh = 250//
 
         if (step === 1){ // if the first step has run, that is if the color was found on the right side, we draw the second square on the right
-            rect (this.x + 10 - width + 10, this.y, this.wd )
+            rect (this.targetx + 10 - width + 10, this.targety, this.wd )
         }
         
         if (findColor(cam, colorToFind, colorrange) === true){ //if the color we are looking has been found within the region on the left, we return the step variable to 0, thus going back to the first step, running things as if the page had just loaded
              step = 0
         }
     }
-} // basically this is touch the targets. if we touch a target on one side, the next one appears, and so on. I am tired :) 
+    hitNotif(){
+        this.notiftext = 'HIT!'
+
+    }
+    greatNotif(){
+        this.notiftext = 'GREAT!!'
+        this.notiftextr = 50;
+        this.notiftextg = 255;
+        this.notiftextb = 10;
+        this.notiftextop = 255;
+
+
+
+        if (hitTimer < 2000){
+            stroke(this.notiftextr, this.notiftextg,this.notiftextb, this.notiftextop)
+            text(this.notiftext, this.notifX, this.notifY)
+            // // // // // the text is also mirrored, gotta fix that
+            this.notiftextop -= 20
+            this.notifY -= 1
+        }
+
+
+    }
+    missNotif(){
+        this.notiftext = 'MISS...'
+
+
+    }
+}
+
